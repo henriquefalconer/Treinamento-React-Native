a69d4e7ebd377a1203aa5cea5d43ecc1b1f0f9c8
consmariano <constanzamrsm@gmail.com>
2020-05-10 19:05:50 -0300

    parte do cadastro pronta

--- src/screens/Forms/SignUpScreen.js
+++ src/screens/Forms/SignUpScreen.js
@@ -5,7 +5,53 @@ import HollowTextField from "../../components/HollowTextField";
 import BlandHeader from "../../components/BlandHeader";
 import FormScreensStyle from '../../style/FormScreens/FormScreensStyle';
 import { useState } from 'react';
-import { signUp } from '../../api/Users'; 
+
+// import { signUp } from '../../api/Users'; 
+
+// function hasError(data) {
+//     return !Object.keys(data).includes('token');
+// }
+
+// async function signUp({ first_name, last_name, username }) {
+//     try {
+//         // Realiza o pedido do tip 'POST' para a API:
+//         let response = await fetch(
+//             'http://piupiuwer.polijr.com.br/usuarios/', 
+//             {
+//                 method: 'POST',
+//                 headers: {
+//                     Accept: 'application/json',
+//                     'Content-Type': 'application/json',
+//                 },
+//                 body: JSON.stringify({
+//                     'first_name': first_name,
+//                     'last_name': last_name,
+//                     'username': username,
+//                 }),
+//             },
+//         );
+
+//         // Decodifica os dados para o formato json:
+//         let data = await response.json();
+
+//         // Imprime os dados obtidos:
+//         console.log(data);
+
+//         if (!hasError(data)) {
+//             // Retorna os dados:
+//             return data;
+//         }
+//         else {
+//             // Retorna null:
+//             return null;
+//         }
+        
+//     } catch (error) {
+//         // Caso haja algum erro, imprima-o e retorne null:
+//         console.error(error);
+//         return null;
+//     }
+// } 
 
 function SignUpScreen({navigation}) {
     const [first_name, setName] = useState('')
@@ -21,30 +67,27 @@ function SignUpScreen({navigation}) {
                     <HollowTextField 
                         value={first_name} 
                         placeholder="Insire seu primeiro nome"
-                        onChange={valor => setName(valor)}
-                        onTextSubmit={() => console.log('nome enviado')}
+                        onChange={NovoNome => setName(NovoNome)}
+                        // onTextSubmit={() => console.log('nome enviado')}
                         
-                        onChange={(NovoNome) => setName(NovoNome)}
                     >
                     </HollowTextField>
 
                     <HollowTextField 
                         value={last_name} 
                         placeholder="Insire seu último nome"
-                        onChange={valor => setLastName(valor)}
-                        onTextSubmit={() => console.log('sobrenome enviado')}
-                        
-                        onChange={(NovoSobrenome) => setLastName(NovoSobrenome)}
+                        onChange={NovoSobrenome => setLastName(NovoSobrenome)}
+                        // onTextSubmit={() => console.log('sobrenome enviado')}
+
                     >
                     </HollowTextField>
 
                     <HollowTextField 
                         value={username} 
                         placeholder="Insire o nome de usuário"
-                        onChange={valor => setUsername(valor)}
-                        onTextSubmit={() => console.log('username enviado')}
+                        onChange={NovoUsuario => setUsername(NovoUsuario)}
+                        // onTextSubmit={() => console.log('username enviado')}
                         
-                        onChange={(NovoUsername) => setUsername(NovoUsername)}
                     >
                     </HollowTextField>
                 </View>
@@ -54,8 +97,13 @@ function SignUpScreen({navigation}) {
                     height={47} 
                     textStyle={FormScreensStyle.continueButtonText} 
                     text="Próximo" 
-                    onPress={
-                        () => {navigation.navigate('CadastroNext');}
+                    onPress=
+                    { 
+                        () => navigation.navigate('CadastroNext', {
+                               first_name:  first_name, 
+                               last_name: last_name, 
+                               username: username 
+                        }) 
                     }
                 />
             </View>
--- src/screens/Forms/SignUpScreenNext.js
+++ src/screens/Forms/SignUpScreenNext.js
@@ -6,9 +6,47 @@ import BlandHeader from "../../components/BlandHeader";
 import FormScreensStyle from '../../style/FormScreens/FormScreensStyle';
 import { useState } from 'react';
 
-function SignUpScreenNext({navigation}) {
+function hasError(data) {
+    return !Objects.keys(data).includes('id')
+}
+// data assume o valor de json quando ela chamar
+
+function signUp ({first_name, last_name, username, email, password}) {
+    return fetch('http://piupiuwer.polijr.com.br/usuarios/', 
+    {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify ({
+            'first_name': first_name, 
+            'last_name': last_name, 
+            'username': username,
+            'email': email,
+            'password': password
+        })
+    }
+    ).then((response) => response.json())
+    .then((json) => {
+        console.log(json)
+        if (hasError(json)) {
+            return "Erro";
+        } else {
+            return null;
+        }
+    })
+    .catch((error) => {
+        return "Erro de conexão";
+        console.log(error)
+    })
+}
+
+function SignUpScreenNext({ route, navigation}) {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [errorTexto, setErrorTexto] = useState('')
+    const  { first_name, last_name, username } = route.params;
 
     return (
         <SafeAreaView style={FormScreensStyle.background}>
@@ -17,33 +55,60 @@ function SignUpScreenNext({navigation}) {
                 <Text style={FormScreensStyle.title}>Cadastro</Text>
         
                 <View>
+                    <Text style={FormScreensStyle.title}>{errorTexto}</Text>
                     <HollowTextField 
                         placeholder="Insire o email"
                         value={email}
-                        onChange = {valor => setEmail(valor)}
-                        onTextSubmit={() => console.log('email enviado')}
+                        onChange = {NovoEmail => setEmail(NovoEmail)}
+                        // onTextSubmit={() => console.log('email enviado')}
                     >
                     </HollowTextField>
+
                     {/* <HollowTextField placeholder="Data de nascimento"></HollowTextField> */}
+
                     <HollowTextField 
                         placeholder="Senha"
                         value={password}
-                        onChange={valor => setPassword(valor)}
-                        onTextSubmit={() => console.log('senha enviada')}
+                        onChange={NovaSenha => setPassword(NovaSenha)}
+                        // onTextSubmit={() => console.log('senha enviada')}
                     >
                     </HollowTextField>
-                    {/* <HollowTextField 
-                        placeholder="Confirme sua senha"
-                        value={passwordConfirm}
-                        onChange={valor => setPasswordConfirm(valor)}
-                    >
-                    </HollowTextField> */}
                 </View>
 
-                <FilledButton width={170} height={47} textStyle={FormScreensStyle.continueButtonText} text="Cadastrar" onPress={() => navigation.navigate('SocialMedia')} />
+                <FilledButton 
+                    width={170} 
+                    height={47} 
+                    textStyle={FormScreensStyle.continueButtonText}
+                    text="Cadastrar" 
+                    onPress= {
+                       async () => {
+                            console.log(first_name);
+                            console.log(last_name);
+                            console.log(username);
+                            console.log(password);
+
+                            const error = await signUp(
+                        { 
+                            first_name: first_name,
+                            last_name: last_name, 
+                            username: username,
+                            email: email,
+                            password: password
+                        });
+                            if (error != null) {
+                                setErrorTexto(error);
+                            }
+                            else {
+                                setErrorTexto('');
+                                navigation.navigate('SocialMedia');
+                            }
+                    }
+                    }
+                />
             </View>
         </SafeAreaView>
     );
 }
 
 export default SignUpScreenNext;
+
