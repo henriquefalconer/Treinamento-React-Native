636696169d1461b07553badf48b963e27fd28f6b
consmariano <constanzamrsm@gmail.com>
2020-05-17 19:40:15 -0300

    colocando pius no profil

--- src/components/SocialMedia/Profile/BoxesNavigation.js
+++ src/components/SocialMedia/Profile/BoxesNavigation.js
@@ -2,21 +2,25 @@ import React from "react";
 import { StyleSheet, Text, View} from "react-native";
 import {TouchableOpacity} from 'react-native-gesture-handler';
 
-function BoxesNavigation({title}) {
+function BoxesNavigation({title, active, onPress}) {
     return (
-        <TouchableOpacity>
-            <View
-                style={{
-                    borderBottomWidth: 2,
-                    borderBottomColor: '#999'
-            }}>
-                <Text style={{
+        <View
+            style={{
+                flex: 1, 
+                borderBottomWidth: active ? 2.5 : 1,
+                borderBottomColor: active ? '#f21d1d' : '#999',
+                justifyContent: 'center',
+            }}
+        >
+        <TouchableOpacity style={{flex: 1}} onPress={() => onPress()}>
+            <Text style={{
                     fontSize: 15,
                     fontWeight: 'bold',
                     textAlign: 'center',
+                    margin: 10,
                 }}>{title}</Text>
-            </View>
         </TouchableOpacity>
+        </View>
     );
 };
 
--- src/screens/SocialMedia/Tabs/ProfileTab.js
+++ src/screens/SocialMedia/Tabs/ProfileTab.js
@@ -1,14 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import { View, StyleSheet, SafeAreaView, TouchableOpacityComponent } from "react-native";
 import { Image, ImageBackground, Text } from "react-native";
-import { TouchableOpacity, ScrollView } from "react-native-gesture-handler";
+import { TouchableOpacity, FlatList, ScrollView } from "react-native-gesture-handler";
 import SocialMediaHeader from "../../../components/SocialMedia/General/SocialMediaHeader";
 import HollowButton from "../../../components/General/HollowButton";
 import PiarButton from "../../../components/SocialMedia/General/PiarButton";
-import { onChange } from "react-native-reanimated";
 import { baseDeDados, loggedInUser } from "../../../utilities/baseDeDados";
+import * as GeneralFunctions from "../../../utilities/GeneralFunctions";
 import WidthFillingImage from "../../../components/SocialMedia/Profile/WidthFillingImage";
 import BoxesNavigation from "../../../components/SocialMedia/Profile/BoxesNavigation";
+import Piu from "../../../components/SocialMedia/Feed/Piu";
 
 //para mudanca de botao
 function changeButton() {
@@ -21,87 +22,122 @@ function changeButton() {
     </TouchableOpacity>
 }
 
+function ProfileTop() {
+    const [abaSelecionada, setabaSelecionada] = useState('Pius');
+    const dadosUsuario = baseDeDados.getDadosUsuarioFromUsername(loggedInUser) == null
+        ? baseDeDados.getDadosUsuarioFromUsername('cleber.cunha')
+        : baseDeDados.getDadosUsuarioFromUsername(loggedInUser);
+    
+        const infoUsuario = dadosUsuario.infoUsuario;
+    
+    return (
+
+    <View>
+        <WidthFillingImage 
+            source = {infoUsuario.background}                
+        />
+        <View style={{top: -20}}>
+            <View style={{paddingHorizontal: 20}}>
+                <View style={{ justifyContent: 'flex-start' }}>
+                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                        <Image
+                            source={infoUsuario.avatar}
+                            style={{
+                                width: 100,
+                                height: 100,
+                                borderRadius: 200,
+                                marginBottom: 12,
+                                backgroundColor: '#ddd',
+                            }}
+                        />
+                        <View style={styles.InfoInteraction}>
+                            <View styles={styles.SeguidoresInteraction}>
+                            <Text style={{ textAlign: 'center', fontWeight: 'bold' }}>{dadosUsuario.getSeguidores().length}</Text>
+                                <Text style={styles.SeguidoresInteractionText}>Seguidores</Text>
+                            </View>
+                            <View styles={styles.SeguindoInteraction}>
+                                <Text style={{ textAlign: 'center', fontWeight: 'bold' }}>
+                                    {infoUsuario.seguindo.length}
+                                </Text>
+                                <Text style={styles.SeguindoInteractionText}>Seguindo</Text>
+                            </View>
+                        </View>
+                    </View>
+                </View>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                    <View>
+                        <Text style={{ fontSize: 20, fontWeight: 'bold' }}>
+                            {infoUsuario.nome}
+                        </Text>
+                        <Text style={{ fontSize: 15, fontWeight: 'normal' }}>
+                            @{infoUsuario.username}
+                        </Text>
+                    </View>
+                    <HollowButton
+                        height={40}
+                        width={110}
+                        textStyle={{
+                            color: '#f21d1d',
+                            fontSize: 17,
+                        }}
+                        text="Seguir"
+                    />
+                </View>
+                <View style={styles.Bio}>
+                    <Text>
+                        {infoUsuario.descricao}
+                    </Text>
+                </View>
+            </View>
+        </View>
+        <View style={styles.BoxesNavigation}>
+                <BoxesNavigation title="Pius" active={abaSelecionada == 'Pius'} onPress={() => setabaSelecionada('Pius')} />
+                <BoxesNavigation title="Pius e Respostas" active={abaSelecionada == 'Pius e Respostas'} onPress={() => setabaSelecionada('Pius e Respostas')} />
+                <BoxesNavigation title="Curtidas" active={abaSelecionada == 'Curtidas'}  onPress={() => setabaSelecionada('Curtidas')}/>    
+        </View>
+    </View>
+    );
+}
 
 
 function ProfileTab({ navigation }) {
-    const dadosUsuario = baseDeDados.getDadosUsuarioFromUsername(loggedInUser);
-    console.log(dadosUsuario);
-    const infoUsuario = dadosUsuario != null 
-        ? dadosUsuario.infoUsuario 
-        : baseDeDados.getDadosUsuarioFromUsername('cleber.cunha').infoUsuario;
-
     return (
         <SafeAreaView style={styles.background} >
             <SocialMediaHeader navigation={navigation} />
             <View style={{ flex: 1, backgroundColor: "#fff" }}>
-                <ScrollView>
-                    <WidthFillingImage 
-                        source = {infoUsuario.background}                
-                    />
-                    <View style={{top: -20}}>
-                        <View style={{paddingHorizontal: 20}}>
-                            <View style={{ justifyContent: 'flex-start' }}>
-                                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                                    <Image
-                                        source={infoUsuario.avatar}
-                                        style={{
-                                            width: 100,
-                                            height: 100,
-                                            borderRadius: 200,
-                                            marginBottom: 12,
-                                        }}
-                                    />
-                                    <View style={styles.InfoInteraction}>
-                                        <View styles={styles.SeguidoresInteraction}>
-                                        <Text style={{ textAlign: 'center', fontWeight: 'bold' }}>{dadosUsuario.getSeguidores().length}</Text>
-                                            <Text style={styles.SeguidoresInteractionText}>Seguidores</Text>
-                                        </View>
-                                        <View styles={styles.SeguindoInteraction}>
-                                            <Text style={{ textAlign: 'center', fontWeight: 'bold' }}>
-                                                {infoUsuario.seguindo.length}
-                                            </Text>
-                                            <Text style={styles.SeguindoInteractionText}>Seguindo</Text>
-                                        </View>
-                                    </View>
-                                </View>
-                            </View>
-                            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                                <View>
-                                    <Text style={{ fontSize: 20, fontWeight: 'bold' }}>
-                                        {infoUsuario.nome}
-                                    </Text>
-                                    <Text style={{ fontSize: 15, fontWeight: 'normal' }}>
-                                        @{infoUsuario.username}
-                                    </Text>
-                                </View>
-                                <HollowButton
-                                    height={40}
-                                    width={110}
-                                    textStyle={{
-                                        color: '#f21d1d',
-                                        fontSize: 17,
-                                    }}
-                                    text="Seguir"
-                                />
-                            </View>
-                            <View style={styles.Bio}>
-                                <Text>
-                                    {infoUsuario.descricao}
-                                </Text>
-                            </View>
-                        </View>
-                        <View style={styles.BoxesNavigation}>
-                            <BoxesNavigation title="Pius" />
-                            <BoxesNavigation title="Pius e Respostas" />
-                            <BoxesNavigation title="Curtidas" />
-                        </View>
-                    </View>
-                </ScrollView>
+                <FlatList
+                    keyExtractor={(key) => {return (key)}} 
+                    ListHeaderComponent={<ProfileTop />}
+                    data={[
+                        GeneralFunctions.createPiuId({
+                            username: "fulano.beltrano",
+                            time: Date.parse("15 Apr 2020 11:38:00"),
+                        }),
+                        GeneralFunctions.createPiuId({
+                            username: "fulano.beltrano",
+                            time: Date.parse("15 Apr 2020 11:38:00"),
+                        }),
+                        GeneralFunctions.createPiuId({
+                            username: "fulano.beltrano",
+                            time: Date.parse("15 Apr 2020 11:38:00"),
+                        }),
+                        GeneralFunctions.createPiuId({
+                            username: "fulano.beltrano",
+                            time: Date.parse("15 Apr 2020 11:38:00"),
+                        }),
+                        GeneralFunctions.createPiuId({
+                            username: "fulano.beltrano",
+                            time: Date.parse("15 Apr 2020 11:38:00"),
+                        }),
+                    ]}
+                    renderItem={({item}) => {
+                    return (
+                        <Piu piuId={item} />
+                    );
+                }}
+                />
             </View>
             <PiarButton navigation={navigation} />
-            <View>
-
-            </View>
         </SafeAreaView>
     );
 };
@@ -110,19 +146,13 @@ const styles = StyleSheet.create({
     background: {
         flex: 1,
     },
-    profileImage: {
-        width: 200,
-        height: 200,
-        borderRadius: 200,
-    },
     Bio: {
         marginTop: 10,
-        marginBottom: 15,
     },
     BoxesNavigation: {
+        flex: 1, 
         flexDirection: 'row',
         justifyContent: 'space-between',
-        alignItems: 'stretch',
     },
     InfoInteraction: {
         flex: 1,
